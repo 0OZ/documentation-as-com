d7a7766a8d186dd4483b9f3ee2f479b62a48b37d
Christian <matschke.berlin@icloud.com>
2023-02-24 16:11:09 +0000

    fix path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://docs.auftrag-select.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/documentation-as-com',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
